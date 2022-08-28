e33d55efb0d936e8a336cf37081fbc55523cc30f
Moises <mcasaverde@unsa.edu.pe>
2022-08-28 17:51:02 -0500

    updated bd

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from urllib import response
 import requests
 
 from flask import Flask, session, redirect, g
@@ -7,6 +8,8 @@ from flask import render_template,url_for
 from flask_cors import CORS, cross_origin # para que no genere errores de CORS al hacer peticiones
 
 from backend.blueprints.evento_blueprint import evento_blueprint
+
+from backend.models.evento import EventoModel
 
 app = Flask(__name__,template_folder='frontend/templates',static_folder='frontend/static')
 
@@ -20,9 +23,9 @@ cors = CORS(app)
 
 @app.route('/', methods=['GET'])
 def Index():
-    response = requests.post("http://127.0.0.1:5000/api/evento/get_all").json() # Testing API
-    print(response[0]['detalles'])
-    return render_template('evento.html')
+    #response = requests.post("http://127.0.0.1:5000/api/evento/get_all").json() # Testing API
+    #print(response[0]['detalles'])
+    return render_template('registrar.html')
 
 
 @app.route('/login', methods=['GET','POST'])
@@ -34,13 +37,17 @@ def Login():
 def Registro():
     return render_template('registrar.html')
 
-@app.route('/evento', methods=['GET'])
-def Evento():
-    return render_template('evento.html')
+@app.route('/evento/<int:id>', methods=['GET'])
+def Evento(id):
+    query = {"id" : id}
+    resp = requests.post("http://127.0.0.1:5000/api/evento/get", json=query).json()
+    return render_template('evento.html', evento=resp)
 
-@app.route('/profile', methods=['GET'])
+@app.route('/profile/<int:id>', methods=['GET'])
 def Profile():
-    return render_template('profile.html')
+    query = {"id" : id}
+    resp = requests.post("http://127.0.0.1:5000/api/ponente/get", json=query).json()
+    return render_template('profile.html', ponente=resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
